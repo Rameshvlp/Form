4f3c0212e1f6e020a82e41cc083444264c0f2ba8
Ramesh <rameshvlp2001@gmail.com>
2023-07-27 23:29:19 +0530

    form

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const tabs =document.querySelectorAll('.tab');
-tabs.forEach(clickTab =>{
-    clickTab.addEventListener('click',()=>{
-        tabs.forEach(tab=> {
-            tab.classList.remove('active');
-        });
-        clickTab.classList.add('active');
-        const clickTabBGcolor=
-        getComputedStyle(clickTab).getPropertyValue('color');
-        console.log(clickTabBGcolor);
-        document.body.style.background=clickTabBGcolor;
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    const signup = document.getElementById("signup");
+    const login = document.getElementById("login");
+
+    switchToSignUp = () => {
+        login.style.transform = "rotate(-5deg)";
+        login.style.animationName = "toRightSignup";
+        login.style.animationDuration = "1s"; // Added "s" for seconds
+
+        setTimeout(() => {
+            login.style.zIndex = "1";
+            signup.style.zIndex = "2";
+            login.style.animationName = "toPositionSignup";
+            login.style.animationDuration = "1s"; // Added "s" for seconds
+        }, 900);
+    };
+
+    switchToLogin = () => {
+        signup.style.animationName = "toRightLogin";
+        signup.style.animationDuration = "1s";
+
+        setTimeout(() => {
+            signup.style.zIndex = "1";
+            login.style.zIndex = "2";
+            signup.style.animationName = "toPosition";
+            signup.style.animationDuration = "1s";
+        }, 900);
+    };
 });
